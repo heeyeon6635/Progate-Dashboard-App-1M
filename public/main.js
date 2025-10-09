7ecafcb5975a5f3a2a23a7390e817b43fcc6592e
heeyeon6635 <145956376+heeyeon6635@users.noreply.github.com>
2025-10-09 13:37:29 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
-<!-- 함수 임포트 --!>
+// 함수 임포트
 import { setupTabs } from "./js/tabs.js";
 import { setupConverter } from "./js/converter.js";
 import { setupFlashcards } from "./js/flashcards.js";
 
-<!-- 이벤트 리스너 추가 --!>
+// 이벤트 리스너 추가
 document.addEventListener("DOMContentLoaded", () => {
   setupTabs();
   setupConverter();
